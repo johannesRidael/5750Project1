a0e755485dbfe7005f53eaaba4819e6c637c4d40
eeeeee <elijah@andersonplanet.net>
2021-10-14 16:01:53 -0600

    fixed loss functions

--- data_generation.py
+++ data_generation.py
@@ -8,6 +8,7 @@ import numpy as np
 from numpy.random import multivariate_normal
 from scipy.linalg.special_matrices import toeplitz
 from scipy import optimize
+import matplotlib.pyplot as plt
 
 n_features = 50  # The dimension of the feature is set to 50
 n_samples = 1000 # Generate 1000 training data
@@ -46,22 +47,17 @@ def sim_logistic_regression(coefs, n_samples=1000, corr=0.5):
 
 
 A, b = sim_logistic_regression(coefs)
-
-#run the 4 methods, and plot
-
-
-#run for Subgradient, proximal gradient, accelrated with momentum, Nesterov's
-for ld in [0.005, 0.01, 0.05, 0.1]:
-    print("lambda: ", ld)
-    for it in [500, 5000, 7500, 9000, 9500, 9750, 9990, 9995, 10000]:
-        print("iterations: ", it)
-
+A=np.array(A)
+b = np.array(b)
+x0=[]
+for i in range(50):
+    x0.append(0)
 
 def F1(x):
-    print("L1 Reg")
+    #print("L1 Reg")
     ret = 0
     for i in range(len(b)):
-        term = 1 + -b[i] * A[i].T @ x
+        term = 1 + math.exp(-b[i] * A[i].T @ x)
         ret += math.log(term, 2)
     ret = ret / (len(b) * 2)
     return ret + ld * np.linalg.norm(x, ord=1)
@@ -72,19 +68,19 @@ def F1_prime(x):
         :return: a vector with the gradient for each x
         currently implemented for log_2
         """
-    ret = []
+    ret = list()
     for i in range(50):
-        curr = None
+        curr = 0
         for j in range(50):
             u = -b[j] * (x @ A[j])
             t = 0
             for k in range(50):
                 t += A[j][k]
-            curr += u * -b[j] * np.exp(u) * (t + A[j][i] * x[i] - A[j][i]) / (np.log(2, np.e) * (1 + np.exp(u)))
+            curr += u * -b[j] * np.exp(u) * (t + A[j][i] * x[i] - A[j][i]) / (math.log(2, np.e) * (1 + np.exp(u)))
         # now we add the gradient of the regularization term
         curr += math.fabs(x[i])
         ret.append(curr)
-    print("L1 Dev")
+    #print("L1 Dev")
     return ret
 
 
@@ -92,7 +88,7 @@ def F2(x):
     print("L2 Reg")
     ret = 0
     for i in range(len(b)):
-        term = 1 + -b[i] * A[i].T @ x
+        term = 1 + math.exp(-b[i] * A[i].T @ x)
         ret += math.log(term, 2)
     ret = ret / (len(b) * 2)
     return ret + ld * np.linalg.norm(x)
@@ -127,9 +123,9 @@ def F2_prime(x):
 def gradient_descent(x0, f, f_prime, hessian=None, adaptative=False):
     #x_i, y_i = x0
     x_i = x0
-    all_x_i = list()
-    all_f_i = list()
-    for i in range(1, 100):
+    all_x_i = []
+    all_f_i = []
+    for i in range(1, 10000):
         all_x_i.append(x_i)
         all_f_i.append(f(x_i))
         dx_i = f_prime(x_i)
@@ -147,6 +143,18 @@ def gradient_descent(x0, f, f_prime, hessian=None, adaptative=False):
         #x_i += - step*dx_i
         for j in range(len(dx_i)):
             x_i[j] -= step*dx_i[j]
-        if np.abs(all_f_i[-1]) < 1e-16:
-            break
     return all_x_i, all_f_i
+
+
+#run the 4(8) methods, and plot
+ld = .001
+exes, wise = gradient_descent(x0, F1, F1_prime)
+print(wise)
+print(exes)
+
+
+#run for Subgradient, proximal gradient, accelrated with momentum, Nesterov's
+for ld in [0.005, 0.01, 0.05, 0.1]:
+    print("lambda: ", ld)
+    for it in [500, 5000, 7500, 9000, 9500, 9750, 9990, 9995, 10000]:
+        print("iterations: ", it)
