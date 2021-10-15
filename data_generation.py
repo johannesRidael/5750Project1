eeec46386e4909368d1364b895c75e05440f6d9b
eeeeee <elijah@andersonplanet.net>
2021-10-15 17:16:55 -0600

    Gradient Descent fully Functional

--- data_generation.py
+++ data_generation.py
@@ -53,17 +53,21 @@ x0=[]
 for i in range(50):
     x0.append(1)
 
-def F1(x):
+def F1(x, lam):
     #print("L1 Reg")
     ret = 0
+    #print("A: ", A)
+    #print("B: ", b)
+    #print("X: ", x)
     for i in range(len(b)):
         hold = -b[i] * A[i].T @ x
         term = 1 + np.exp(hold)
         ret += math.log(term, 2)
     ret = ret / (len(b) * 2)
-    return (ret/(2*len(x))) + ld * np.linalg.norm(x, ord=1)
+    #print("ret: ", ret)
+    return (ret/(2*len(x))) + lam * np.linalg.norm(x, ord=1)
 
-def F1_prime(x):
+def F1_prime(x, lam):
     """
         :x: a vector d=50
         :return: a vector with the gradient for each x
@@ -81,57 +85,73 @@ def F1_prime(x):
             curr += u * -b[j] * np.exp(u) * (t + A[j][i] * x[i] - A[j][i]) / (math.log(2, np.e) * (1 + np.exp(u)))
         # now we add the gradient of the regularization term
         curr = curr / (2 * len(x))
-        curr += 1
-        ret.append(curr)
+        curr += lam
+        if math.isnan(curr):
+            ret.append(0)
+        else:
+            ret.append(curr)
     #print("L1 Dev")
+    #print("dev: ", ret)
     return ret
 
 
-def F2(x):
-    print("L2 Reg")
+def F2(x, lam):
+    # print("L1 Reg")
     ret = 0
+    # print("A: ", A)
+    # print("B: ", b)
+    # print("X: ", x)
     for i in range(len(b)):
-        term = 1 + math.exp(-b[i] * A[i].T @ x)
+        hold = -b[i] * A[i].T @ x
+        term = 1 + np.exp(hold)
         ret += math.log(term, 2)
     ret = ret / (len(b) * 2)
-    return ret + ld * np.linalg.norm(x)
+    # print("ret: ", ret)
+    return ret + lam * np.linalg.norm(x)
 
-def F2_prime(x):
+def F2_prime(x, lam):
     """
         :x: a vector d=50
         :return: a vector with the gradient for each x
         currently implemented for log_2
         """
-    ret = []
+    ret = list()
     for i in range(50):
-        curr = None
+        curr = 0
         for j in range(50):
             u = -b[j] * (x @ A[j])
             t = 0
             for k in range(50):
                 t += A[j][k]
-            curr += u * -b[j] * np.exp(u) * (t + A[j][i] * x[i] - A[j][i]) / (np.log(2, np.e) * (1 + np.exp(u)))
+            a = (math.log(2, np.e) * (1 + np.exp(u)))
+            curr += u * -b[j] * np.exp(u) * (t + A[j][i] * x[i] - A[j][i]) / (math.log(2, np.e) * (1 + np.exp(u)))
+        # now we add the gradient of the regularization term
+        curr = curr / (2 * len(x))
         # now we add the gradient of the regularization term
         u = 0
         for k in range(50):
             u += x[k] * x[k]
-        curr += x[i] / np.sqrt(u)
-        ret.append(curr)
-    print("L2 Dev")
+        curr += lam * x[i] / np.sqrt(u)
+        if math.isnan(curr):
+            ret.append(0)
+        else:
+            ret.append(curr)
+    #print("L2 Dev")
     return ret
 
 
 #from https://scipy-lectures.org/advanced/mathematical_optimization/auto_examples/plot_gradient_descent.html
-#TODO--Convert to arbitrary dimension,
-def gradient_descent(x0, f, f_prime, hessian=None, adaptative=False):
+#Changed--Converted to arbitrary dimension,
+def gradient_descent(x0, f, f_prime, lam, hessian=None, adaptative=False):
     #x_i, y_i = x0
     x_i = x0
     all_x_i = []
     all_f_i = []
     for i in range(1, 10000):
         all_x_i.append(x_i)
-        all_f_i.append(f(x_i))
-        dx_i = f_prime(x_i)
+        all_f_i.append(f(x_i, lam))
+        dx_i = f_prime(x_i, lam)
+        ints.append(i)
         if adaptative:
             # Compute a step size using a line_search to satisfy the Wolf
             # conditions
@@ -142,24 +162,40 @@ def gradient_descent(x0, f, f_prime, hessian=None, adaptative=False):
             if step is None:
                 step = 0
         else:
-            step = 1
+            step = 0.0001
         #x_i += - step*dx_i
         for j in range(len(dx_i)):
             x_i[j] -= step*dx_i[j]
-        if math.fabs(f(x_i) - all_f_i[i-1]) < 1e-16:
+        #print(f(x_i,), " - ", all_f_i[i-1], " = ", f(x_i) - all_f_i[i-1])
+        if (all_f_i[i-1] - f(x_i, lam)) < 1e-12:
             break
     return all_x_i, all_f_i
 
 
 #run the 4(8) methods, and plot
 ld = .001
-exes, wise = gradient_descent(x0, F1, F1_prime)
-print(wise)
-print(exes)
+ints = []
+exes, wise = gradient_descent(x0.copy(), F1, F1_prime, ld)
+
+figure, axis = plt.subplots(2, 1)
+axis[0].plot(ints, wise)
+ints = []
+exes, wise = gradient_descent(x0.copy(), F2, F2_prime, ld)
+axis[1].plot(ints, wise)
+#print(wise)
+#print(exes)
 
 
 #run for Subgradient, proximal gradient, accelrated with momentum, Nesterov's
-for ld in [0.005, 0.01, 0.05, 0.1]:
-    print("lambda: ", ld)
-    for it in [500, 5000, 7500, 9000, 9500, 9750, 9990, 9995, 10000]:
-        print("iterations: ", it)
+for l in [0.005, 0.01, 0.05, 0.1]:
+    print("lambda: ", l)
+    x1, y1 = gradient_descent(x0.copy(), F1, F1_prime, l)
+    x2, y2= gradient_descent(x0.copy(), F2, F2_prime, l)
+    len1 = len(x1)
+    len2 = len(x2)
+    for s in [.02, .1, .5, .8, .9, .97, .98, .99]:
+        print("index: ", math.floor(s*len1), x1[math.floor(s*len1)])
+    for s in [.02, .1, .5, .8, .9, .97, .98, .99]:
+        print("index: ", math.floor(s * len2), x2[math.floor(s * len2)])
+
+plt.show()
